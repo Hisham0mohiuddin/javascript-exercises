a26af5f8d41a58518e9e8eaf1df404e520c36bf6
Hisham0mohiuddin <user@Xubuntu.myguest.virtualbox.org>
2024-12-23 18:46:47 +0530

    the final

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(w) {
+    let n = w.length;
+    let ans="";
+    for (let i=n-1;i>=0;i--){
+        ans+=w[i];
+    }
+    return ans;
 };
 
 // Do not edit below this line
