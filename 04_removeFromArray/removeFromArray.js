f0a48fbefd1b218b7df72c6df97b89c6419cbe05
Hisham0mohiuddin <user@Xubuntu.myguest.virtualbox.org>
2024-12-23 19:17:50 +0530

    the end

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    let ans=[];
+    for (let i=0;i<array.length;i++){
+        for(let j of args){
+            if(j===array[i]){
+                array.splice(i,1);
+                i--;
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
