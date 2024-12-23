7f4bacab7108b009d3c011a3f1bd0740376d6444
Hisham0mohiuddin <user@Xubuntu.myguest.virtualbox.org>
2024-12-23 19:28:27 +0530

    the final

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,22 @@
-const sumAll = function() {
+const sumAll = function(x,y) {
+    if (x<0||y<0){
+        return "ERROR";
+    }
+    let b= Math.floor(x);
+    if(b!=x){
+        return "ERROR";
+    }
+    if (y.length>1){
+        return "ERROR";
+    }
+    if(x>y){
+        [x,y]=[y,x];
+    }
+    let sum =0;
+    for(let i=x;i<=y;i++){
+        sum+=i;
+    }
+    return sum;
 
 };
 
