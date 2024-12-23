8a1254365829c50303c371eb63bfde37b1cb5326
Hisham0mohiuddin <user@Xubuntu.myguest.virtualbox.org>
2024-12-23 18:37:53 +0530

    the is the final

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(w,n) {
+    if (n<0) return "ERROR";
+    let sol ="";
+    for(let i =0;i<n;i++){
+        sol +=w;
+    } 
+    return sol;
 };
 
 // Do not edit below this line
